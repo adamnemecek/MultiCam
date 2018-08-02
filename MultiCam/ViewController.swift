01d89f3172388f575a375edc40e6b17835a9a558
Adam Nemecek <adamnemecek@gmail.com>
2018-08-02 09:34:32 -0700

    whitespace

--- MultiCam/ViewController.swift
+++ MultiCam/ViewController.swift
@@ -10,9 +10,9 @@ import Cocoa
 import AVFoundation
 
 class ViewController: NSViewController {
-    
+
     @IBOutlet weak var previewBox: NSView!
-    
+
     let session = AVCaptureSession()
     var deviceInput: AVCaptureDeviceInput?
     var videoDataOutput: AVCaptureVideoDataOutput?
@@ -27,98 +27,98 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-    
+
     //get all video devices
     func getDevices() -> [AVCaptureDevice] {
-        
+
         let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo) as! [AVCaptureDevice]
-        
+
         for dev in devices {
             NSLog(dev.modelID)
         }
-        
+
         return devices
     }
-    
+
     //calculate rectangle position
     //the videos are arranged in a row
     func calcRect(currentDevice: Int, deviceCount: Int) -> CGRect {
-        
-        
+
+
         let index: CGFloat = CGFloat(currentDevice - 1)
-        
+
         let rectHeight = previewBox.bounds.height
         let rectWidth = previewBox.bounds.width / CGFloat(deviceCount)
-        
+
         let rectX: CGFloat = index * rectWidth
         let rectY: CGFloat = 0
-        
+
         let rect = CGRect(x: rectX, y: rectY, width: rectWidth, height: rectHeight)
-        
+
         return rect
     }
-    
+
     //setup session with inputs and output
     func setupSession() {
         do {
 
             session.beginConfiguration()
-            
+
             let devices = getDevices()
             let deviceCount = devices.count
-            
+
             let superVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
-            
+
             var currentDevice: Int = 0
-            
+
             for device in devices {
-                
+
                 //device number
                 currentDevice += 1
-                
+
                 //adding devices as input to session
                 let deviceInput = try AVCaptureDeviceInput(device: device)
                 if (session.canAddInput(deviceInput) == true) {
                     session.addInputWithNoConnections(deviceInput)
                     NSLog("\(device.modelID) added as deviceInput")
                 }
-                
+
                 //get the video input port
                 let inputPort = deviceInput.ports[0] as! AVCaptureInputPort
-                
+
                 //create a video preview layer with the session
                 let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
-                
+
                 //create a connection with the input port and the preview layer
                 //and connect it to the session
                 let connection = AVCaptureConnection(inputPort: inputPort, videoPreviewLayer: videoPreviewLayer)
-                
+
                 if (session.canAddConnection(connection) == true) {
                     session.addConnection(connection)
                 }
-                
+
                 videoPreviewLayer.frame = calcRect(currentDevice, deviceCount: deviceCount)
-                
+
                 superVideoPreviewLayer.addSublayer(videoPreviewLayer)
-                
+
             }
-            
-            
+
+
             //connect layer to view
             previewBox.layer = superVideoPreviewLayer
-            
+
 /*            deviceInput = try AVCaptureDeviceInput(device: device)
-            
-            
+
+
             if (session.canAddInput(deviceInput) == true) {
                 session.addInput(deviceInput)
                 NSLog("input added")
             }
 
-            
+
             videoDataOutput = AVCaptureVideoDataOutput()
             videoDataOutput?.alwaysDiscardsLateVideoFrames = true
-            
+
             if (session.canAddOutput(videoDataOutput) == true) {
                 session.addOutput(videoDataOutput)
                 NSLog("output added")
@@ -136,7 +136,7 @@ class ViewController: NSViewController {
         catch let error as NSError {
             NSLog("\(error), \(error.localizedDescription)")
         }
-        
+
     }
 }
 
